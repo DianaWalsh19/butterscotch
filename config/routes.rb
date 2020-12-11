a55c6ee64e1b4b3f580f3f239efec91fe398a095
DianaWalsh19 <dianawalsh19@gmail.com>
2020-12-11 20:52:15 +0000

    Payment simulation is working now

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   
   get '/checkout' => 'cart#createOrder'
   
+  get '/paid/:id' => 'static_pages#paid'
+  
   get '/cart' => 'cart#index'
   
   resources :items
